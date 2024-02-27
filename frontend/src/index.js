0d154f9bf5c354e3f6498435bafcd2787b7fd755
Chandan <hb7102849@gmail.com>
2024-02-27 15:29:19 +0530

    redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
 import reportWebVitals from './reportWebVitals';
+import appStore from './utils/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={appStore} >
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
