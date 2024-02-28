e7cfd12f0191d7c9923f0e4d444d2d085c214148
Chandan <hb7102849@gmail.com>
2024-02-28 17:45:56 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(fileupload({
 app.use(express.urlencoded({extended:false}))
 app.use(
     cors({
-        origin:["http://localhost:3000","http://localhost:1234"],
+        origin:["http://localhost:3000","http://localhost:1234","https://in-bulk-chandan-assignment.vercel.app/",],
         methods:["POST", "GET", "PUT", "DELETE"],
         credentials:true,
     })
